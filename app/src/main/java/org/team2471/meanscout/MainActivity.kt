8247df382f848bdd8dea4b7b021cc3800829d545
Aidunlin <aidunlin@gmail.com>
2019-09-07 20:25:21 -0700

    Shorten view variable names

--- app/src/main/java/org/team2471/meanscout/MainActivity.kt
+++ app/src/main/java/org/team2471/meanscout/MainActivity.kt
@@ -15,22 +15,22 @@ import java.io.FileWriter
 import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var teamView: EditText
-    private lateinit var matchView: EditText
-    private lateinit var noshowView: Button
-    private lateinit var confNoshowView: Button
-    private lateinit var supTubView: CheckBox
-    private lateinit var collBunnyIncView: Button
-    private lateinit var collBunnyDecView: Button
-    private lateinit var touchTubIncView: Button
-    private lateinit var touchTubDecView: Button
-    private lateinit var spoiledBunnyView: CheckBox
-    private lateinit var collCubeIncView: Button
-    private lateinit var collCubeDecView: Button
-    private lateinit var penaltyView: RadioGroup
-    private lateinit var driveRatingView: RatingBar
-    private lateinit var commentView: EditText
-    private lateinit var breakdownView: EditText
+    private lateinit var tView: EditText   // Team
+    private lateinit var mView: EditText   // Match
+    private lateinit var nsView: Button    // No Show
+    private lateinit var cnsView: Button   // Confirm No Show
+    private lateinit var stView: CheckBox  // Supported Tub
+    private lateinit var biView: Button    // Bunny Increment
+    private lateinit var bdView: Button    // Bunny Decrement
+    private lateinit var tiView: Button    // Tub Increment
+    private lateinit var tdView: Button    // Tub Decrement
+    private lateinit var sbView: CheckBox  // Spoiled Bunny
+    private lateinit var ciView: Button    // Cube Increment
+    private lateinit var cdView: Button    // Cube Decrement
+    private lateinit var pView: RadioGroup // Penalty
+    private lateinit var dView: RatingBar  // Driving
+    private lateinit var cView: EditText   // Comment
+    private lateinit var bView: EditText   // Breakdown
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (ContextCompat.checkSelfPermission(
@@ -54,23 +54,23 @@ class MainActivity : AppCompatActivity() {
             )
         }
         setContentView(R.layout.activity_main)
-        teamView = findViewById(R.id.team)
-        matchView = findViewById(R.id.match)
-        matchView.setText(match.toString())
-        noshowView = findViewById(R.id.noshow)
-        confNoshowView = findViewById(R.id.confNoshow)
-        supTubView = findViewById(R.id.supTub)
-        collBunnyIncView = findViewById(R.id.collBunnyInc)
-        collBunnyDecView = findViewById(R.id.collBunnyDec)
-        touchTubIncView = findViewById(R.id.touchTubInc)
-        touchTubDecView = findViewById(R.id.touchTubDec)
-        spoiledBunnyView = findViewById(R.id.spoiledBunny)
-        collCubeIncView = findViewById(R.id.collCubeInc)
-        collCubeDecView = findViewById(R.id.collCubeDec)
-        penaltyView = findViewById(R.id.penalty)
-        driveRatingView = findViewById(R.id.driveRating)
-        commentView = findViewById(R.id.comment)
-        breakdownView = findViewById(R.id.breakdown)
+        tView = findViewById(R.id.team)
+        mView = findViewById(R.id.match)
+        mView.setText(match.toString())
+        nsView = findViewById(R.id.noshow)
+        cnsView = findViewById(R.id.confNoshow)
+        stView = findViewById(R.id.supTub)
+        biView = findViewById(R.id.collBunnyInc)
+        bdView = findViewById(R.id.collBunnyDec)
+        tiView = findViewById(R.id.touchTubInc)
+        tdView = findViewById(R.id.touchTubDec)
+        sbView = findViewById(R.id.spoiledBunny)
+        ciView = findViewById(R.id.collCubeInc)
+        cdView = findViewById(R.id.collCubeDec)
+        pView = findViewById(R.id.penalty)
+        dView = findViewById(R.id.driveRating)
+        cView = findViewById(R.id.comment)
+        bView = findViewById(R.id.breakdown)
     }
 
     private var match = 1
@@ -78,20 +78,20 @@ class MainActivity : AppCompatActivity() {
     private var tubsTouched = 0
     private var cubesCollected = 0
     fun crement(view: View) {
-        if (view == collBunnyIncView && bunniesCollected < 6) bunniesCollected++
-        if (view == touchTubIncView && tubsTouched < 8) tubsTouched++
-        if (view == collCubeIncView && cubesCollected < 120) cubesCollected++
-        if (view == collBunnyDecView && bunniesCollected > 0) bunniesCollected--
-        if (view == touchTubDecView && tubsTouched > 0) tubsTouched--
-        if (view == collCubeDecView && cubesCollected > 0) cubesCollected--
-        collBunnyIncView.text = bunniesCollected.toString()
-        touchTubIncView.text = tubsTouched.toString()
-        collCubeIncView.text = cubesCollected.toString()
+        if (view == biView && bunniesCollected < 6) bunniesCollected++
+        if (view == tiView && tubsTouched < 8) tubsTouched++
+        if (view == ciView && cubesCollected < 120) cubesCollected++
+        if (view == bdView && bunniesCollected > 0) bunniesCollected--
+        if (view == tdView && tubsTouched > 0) tubsTouched--
+        if (view == cdView && cubesCollected > 0) cubesCollected--
+        biView.text = bunniesCollected.toString()
+        tiView.text = tubsTouched.toString()
+        ciView.text = cubesCollected.toString()
     }
 
     fun onNoShow(@Suppress("UNUSED_PARAMETER") view: View) {
-        noshowView.visibility = View.INVISIBLE
-        confNoshowView.visibility = View.VISIBLE
+        nsView.visibility = View.INVISIBLE
+        cnsView.visibility = View.VISIBLE
     }
 
     fun confNoShow(@Suppress("UNUSED_PARAMETER") view: View) {
@@ -103,21 +103,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun submit(show: Boolean) {
-        noshowView.visibility = View.VISIBLE
-        confNoshowView.visibility = View.INVISIBLE
+        nsView.visibility = View.VISIBLE
+        cnsView.visibility = View.INVISIBLE
         val sb = StringBuilder()
-        sb.append(teamView.text).append(",").append(matchView.text)
+        sb.append(tView.text, ",", mView.text)
         val filename = sb.toString()
-        sb.append(",").append(show).append(",").append(supTubView.isChecked)
-        sb.append(",").append(bunniesCollected).append(",").append(tubsTouched)
-        sb.append(",").append(spoiledBunnyView.isChecked).append(",").append(cubesCollected)
-        when (penaltyView.checkedRadioButtonId) {
+        sb.append(",", show, ",", stView.isChecked)
+        sb.append(",", bunniesCollected, ",", tubsTouched)
+        sb.append(",", sbView.isChecked, ",", cubesCollected)
+        when (pView.checkedRadioButtonId) {
             2131230851 -> sb.append(",none")
             2131230854 -> sb.append(",yellow")
             2131230852 -> sb.append(",red")
         }
-        sb.append(",").append(driveRatingView.rating)
-        sb.append(",").append(commentView.text).append(",").append(breakdownView.text)
+        sb.append(",", dView.rating)
+        sb.append(",", cView.text, ",", bView.text)
         val file = File(getExternalFilesDir(null), "$filename.txt")
         file.writeText(sb.toString())
         val fw = BufferedWriter(FileWriter(file, false))
@@ -128,24 +128,24 @@ class MainActivity : AppCompatActivity() {
             arrayOf(file.toString()), arrayOf("text/plain"), null
         )
         match = try {
-            Integer.parseInt(matchView.text.toString()) + 1
+            Integer.parseInt(mView.text.toString()) + 1
         } catch (e: NumberFormatException) {
             1
         }
-        matchView.setText(match.toString())
-        teamView.setText("")
-        supTubView.isChecked = false
-        collBunnyIncView.text = "0"
+        mView.setText(match.toString())
+        tView.setText("")
+        stView.isChecked = false
+        biView.text = "0"
         bunniesCollected = 0
-        touchTubIncView.text = "0"
+        tiView.text = "0"
         tubsTouched = 0
-        spoiledBunnyView.isChecked = false
-        collCubeIncView.text = "0"
+        sbView.isChecked = false
+        ciView.text = "0"
         cubesCollected = 0
-        penaltyView.check(R.id.penaltyNone)
-        driveRatingView.rating = 0.0f
-        commentView.setText("")
-        breakdownView.setText("")
+        pView.check(R.id.penaltyNone)
+        dView.rating = 0.0f
+        cView.setText("")
+        bView.setText("")
         Toast.makeText(applicationContext, "Submitted", Toast.LENGTH_SHORT).show()
     }
 }
